e218cf8e232a95c4f605411bded1c41553f2dd9c
Bruno FROGER <bruno.froger@orange.com>
2018-03-14 21:02:39 +0100

    update unsubscribe

--- index.php
+++ index.php
@@ -10,7 +10,33 @@ session_start();
 $myIncludePath = '/var/www/html/dictacloud';
 set_include_path(get_include_path() . PATH_SEPARATOR . $myIncludePath); 
 
-header( 'content-type: text/html; charset=utf-8' );
+//header( 'content-type: text/html; charset=utf-8' );
+
+
+
+//Make sure that it is a POST request.
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
+    throw new Exception('Request method must be POST!');
+}
+ 
+//Make sure that the content type of the POST request has been set to application/json
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if(strcasecmp($contentType, 'application/json') != 0){
+    throw new Exception('Content type must be: application/json');
+}
+ 
+//Receive the RAW post data.
+$content = trim(file_get_contents("php://input"));
+ 
+//Attempt to decode the incoming RAW post data from JSON.
+$decoded = json_decode($content, true);
+ 
+//If json_decode failed, the JSON is invalid.
+if(!is_array($decoded)){
+    throw new Exception('Received content contained invalid JSON!');
+}
+
+
 
 $vide=" ";
 
