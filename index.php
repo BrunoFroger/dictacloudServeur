f5bb40a9f006ba91cabb94284619e7b5842bc443
Bruno FROGER <bruno.froger@orange.com>
2018-03-15 20:05:30 +0100

    update json request

--- index.php
+++ index.php
@@ -27,8 +27,12 @@ if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
 //Make sure that the content type of the POST request has been set to application/json
 $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 if(strcasecmp($contentType, 'application/json') != 0){
-    throw new Exception('Content type must be: application/json');
-    //echo "Content type must be: application/json\n";
+    if (strcasecmp($contentType, 'application/octet-stream') == 0){
+        include_once 'controleurs/storePhoto2.php';
+    }else{
+        throw new Exception('Content type must be: application/json');
+        //echo "Content type must be: application/json\n";
+    }
 }
  
 //echo "etape 2 index.php\n";
